c7c70008dca690962c401d948729e05a58237f4a
Chris Hopkins <hopkinsc4@gmail.com>
2017-09-22 11:21:11 -0400

    Audio src change

--- js/audio-control.js
+++ js/audio-control.js
@@ -1,6 +1,6 @@
 var aud = document.getElementById("myAudio");
 
 function playAudio() {
-    aud.src = "music/TheStLouisBlues.mp3";
+    aud.src = "music/Pipe_Choir_-_09_-_Say_It_Anyway";
     aud.load();
 }
